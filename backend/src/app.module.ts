58fbebe87c6de76a9b1e9df57c7dd581c461ad9b
Christopher Lin <chrislin90210@gmail.com>
2025-10-17 13:28:44 -0500

    env var

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebAppController } from './webapp/webapp.controller';
 import { ScheduleModule } from '@nestjs/schedule';
 import { JobModule } from './jobs/jobs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), JobModule],
+  imports: [ScheduleModule.forRoot(), JobModule, ConfigModule.forRoot()],
   controllers: [WebAppController],
   providers: [],
 })
--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 // import { spawn } from 'child_process';
 
 @Injectable()
 export class JobService {
-  constructor() {
-    dotenv.config();
-  }
+  constructor(private configService: ConfigService) {}
   //TODO what's update freq of db?
   //every minute
   @Cron('0 * * * * *')
   getNYData() {
-    // const path: string = process.env['NY_SCRAPER_PATH'] ?? 'TODO';
+    // const path: string = this.configService.get<string>('NY_SCRAPER_PATH') ?? 'TODO';
     //TODO how to start scraper?
     // spawn('python', [path]);
     console.log('get data');
