d1406f16e8d59fa2717b67ecaf818885a8d13a18
Chris Lin <chrislin90210@gmail.com>
2025-10-20 01:08:15 -0500

    fix JobModule

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobService } from './jobs.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [JobService],
-  exports: [JobService],
+  // exports: [JobService],
 })
 export class JobModule {}
