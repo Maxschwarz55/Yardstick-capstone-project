abd553789340e520735f56060c27742b1da4e634
Christopher Lin <chrislin90210@gmail.com>
2025-10-21 17:52:55 -0500

    remove old backend files

--- dist/db/queries.d.ts
+++
@@ -1,3 +0,0 @@
-declare function testQuery(): Promise<any[]>;
-export { testQuery };
-//# sourceMappingURL=queries.d.ts.map
--- dist/routes/TestRouter.d.ts
+++
@@ -1,3 +0,0 @@
-declare const testRouter: import("express-serve-static-core").Router;
-export default testRouter;
-//# sourceMappingURL=TestRouter.d.ts.map
